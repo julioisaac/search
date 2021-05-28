1184f3030e84aafc756421a4f9c598a4f5c7b5e9
Julio Isaac <julioisaac7@gmail.com>
2021-05-28 16:55:08 -0300

    ajuste no log de retorno da busca

--- src/main/java/br/com/luizalabs/search/Searcher.java
+++ src/main/java/br/com/luizalabs/search/Searcher.java
@@ -17,22 +17,22 @@ public class Searcher {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         final Map<String, Set<String>> index = IndexService.getInstance().index();
 
-        long startTime = System.nanoTime();
-
         Validation.sentence(args);
         Validation.index(index);
         String[] words = Validation.checkSameWords(args[0]);
+
+        long startTime = System.nanoTime();
+
         var searcher = new FindService(index);
         List<String> results = searcher.findByTerms(words);
 
-        long elapsedTime = (System.nanoTime() - startTime);
-        double elapsedTimeInSecond = (double) elapsedTime / 1000_000_000;
+        var elapsedTime = (System.nanoTime() - startTime);
+        var elapsedTimeInSecond = (double) elapsedTime / 1000_000_000;
 
         var terms = String.join(" ", words);
         var occurrences = String.join("\n", results);
 
         logger.log(Level.INFO, Msg.OUTPUT, new Object[]{results.size(), terms, terms, occurrences});
-        logger.log(Level.INFO, "{0} nanoseconds", elapsedTime);
-        logger.log(Level.INFO, "{0} s", elapsedTimeInSecond);
+        logger.log(Level.INFO, "Time elapsed in search\n{0} nanoseconds\n{1} s", new Object[]{elapsedTime, elapsedTimeInSecond});
     }
 }
