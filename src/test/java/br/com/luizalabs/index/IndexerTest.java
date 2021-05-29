16addf239c6c3d84a6ddd2956bb98d49c4132b75
Julio Isaac <julioisaac7@gmail.com>
2021-05-28 22:14:04 -0300

    desabilitando variaveis de ambiente nos testes

--- src/test/java/br/com/luizalabs/index/IndexerTest.java
+++ src/test/java/br/com/luizalabs/index/IndexerTest.java
@@ -1,12 +1,14 @@
 package br.com.luizalabs.index;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndexerTest {
 
     @Test
+    @DisabledIfEnvironmentVariable(named = "ORIGIN_DATA_PATH", matches = ".*")
     void shouldThrowNullPointerException() {
         var exception = assertThrows(NullPointerException.class, () -> Indexer.main(new String[] {}));
         assertEquals("Missing environment variable ORIGIN_DATA_PATH", exception.getMessage());
--- src/test/java/br/com/luizalabs/search/SearcherTest.java
+++ src/test/java/br/com/luizalabs/search/SearcherTest.java
@@ -1,12 +1,14 @@
 package br.com.luizalabs.search;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearcherTest {
 
     @Test
+    @DisabledIfEnvironmentVariable(named = "SEARCH_INDEX_PATH", matches = ".*")
     void justCheckEnv() {
         var exception = assertThrows(NullPointerException.class, () -> Searcher.main(new String[] {}));
         assertEquals("Missing environment variable SEARCH_INDEX_PATH", exception.getMessage());
